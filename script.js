af268edeaf42b1271073758c6a3b2a1ef2bef1c4
tuana-0 <tualhaidari@gmail.com>
2024-10-31 22:54:18 +0300

    Add computerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(`Hello`);
+let computerChoice = '';
+
+function getComputerChoice() {
+    let random = Math.floor(Math.random()*(3 - 1 + 1) + 1);
+    if (random === 1) {
+        return 'rock'
+    }
+    else if (random === 2) {
+        return 'paper'
+    }
+    else {
+        return 'scissor'
+    }
+
+}
+
+
+
+console.log(getComputerChoice())
